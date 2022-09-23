d1480dc210641b5fcc4d853b9486aeb65a183d3f
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-23 12:14:22 +0700

    .

--- proto/src/draw.cpp
+++ proto/src/draw.cpp
@@ -1,9 +1,14 @@
 #include <draw.hpp>
 
-void draw(pos position, pixel pix, fb framebuffer){
-    int offset =  position.x * framebuffer.vinfo.bits_per_pixel/8 +
-        position.y * framebuffer.finfo.line_length;
-    framebuffer.data[offset+2] = pix.r;
-    framebuffer.data[offset+1] = pix.g;
-    framebuffer.data[offset+0] = pix.b;
+void draw(pos position, pixel pix, fb framebuffer){  
+    int offset = 
+        (position.x+framebuffer.vinfo.xoffset) *
+        (framebuffer.vinfo.bits_per_pixel/8) +
+        (position.y+framebuffer.vinfo.yoffset) *
+        framebuffer.finfo.line_length;
+   // cout << offset << endl;
+    framebuffer.data[ offset    ] = pix.b;
+    framebuffer.data[ offset + 1] = pix.g;
+    framebuffer.data[ offset + 2] = pix.r; 
+    framebuffer.data[ offset + 3] = 0;
 }
--- proto/src/main.cpp
+++ proto/src/main.cpp
@@ -6,14 +6,15 @@ int main(){
     const char* fbdev = "/dev/fb0";
     fb framebuffer = open_fb(fbdev);
     //clearscreen_fb(framebuffer);
-    for (int y=1;y<framebuffer.vinfo.yres;y++){
-        for (int x=1;x<framebuffer.finfo.line_length;x++){
+    //cout << framebuffer.vinfo.bits_per_pixel;
+    for (int y=1;y<framebuffer.vinfo.yres_virtual;y++){
+        for (int x=0;x<framebuffer.vinfo.xres_virtual;x++){
             pos position=pos(x,y);
-            pixel pix=pixel(240, 240, 125);
+            pixel pix=pixel(240, 128, 1);
             draw(position, pix, framebuffer);
         }
     }
-    
+
     unload_fb(framebuffer);
     return 0;
 }
