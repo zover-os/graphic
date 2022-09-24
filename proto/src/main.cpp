029b06a196d5c3907b03eab97ff21341ccc0c277
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-24 19:58:54 +0700

    example drawing (1000 frames for 52 seconds)

--- proto/src/main.cpp
+++ proto/src/main.cpp
@@ -7,13 +7,16 @@ int main(){
     fb framebuffer = open_fb(fbdev);
     //clearscreen_fb(framebuffer);
     //cout << framebuffer.vinfo.bits_per_pixel;
-    for (int x=0;x<framebuffer.vinfo.xres_virtual;x++){
-        for (int y=1;y<framebuffer.vinfo.yres_virtual;y++){
-            pos position=pos(x,y);
-            pixel pix=pixel(0, 240, 0);
-            draw(position, pix, framebuffer);
+    for (int d=1;d<1000;d++){
+        for (int x=0;x<framebuffer.vinfo.xres_virtual;x++){
+            for (int y=1;y<framebuffer.vinfo.yres_virtual;y++){
+                pos position=pos(x,y);
+                pixel pix=pixel((x-d)%256, (x+y-d)%256, (y-d)%256);
+                draw(position, pix, framebuffer);
+            }
         }
-    }
+        //cout << d << endl;
+    } 
 
     unload_fb(framebuffer);
     return 0;
